e7237b4daae673810bb6a4420995d4a1f6ced6c8
Richie-Z <zakariarichie@yahoo.com>
2026-09-18 23:13:41 +0700

    feat: migrate from oxlint to biome for linting and formatting

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,24 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
-    'subject-min-length': [2, 'always', 1],
-    'subject-max-length': [2, 'always', 100],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    "subject-min-length": [2, "always", 1],
+    "subject-max-length": [2, "always", 100],
   },
-}
+};
