fad77e7f7ffc1ecb1f9d56377c21f9994cc90b60
Richie-Z <zakariarichie@yahoo.com>
2026-09-18 23:04:06 +0700

    feat: fix git hooks for husky v9

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
+    'subject-min-length': [2, 'always', 1],
+    'subject-max-length': [2, 'always', 100],
+  },
+}
